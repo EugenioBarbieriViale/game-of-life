9aa3679d1df1ad0c6c18f3084456c7981631d53b
EugenioBarbieriViale <eugenio.barbieri.viale@gmail.com>
2024-08-17 12:44:13 +0200

    WORKING MINIMAL

--- main.c
+++ main.c
@@ -2,14 +2,19 @@
 #include <raylib.h>
 #include <unistd.h>
 
-void draw_grid(int L, int size) {
+#define L 800
+#define size 15
+
+#define l (int)(L/size)
+
+void draw_grid() {
     for (int i=0; i < L; i+=size) {
         DrawLine(i, 0, i, L, WHITE);
         DrawLine(0, i, L, i, WHITE);
     }
 }
 
-void alloc_cells(int l, int cells[l][l]) {
+void alloc_cells(int cells[l][l]) {
     for (int i=0; i<l; i++) {
         for (int j=0; j<l; j++) {
             cells[i][j] = 0;
@@ -17,12 +22,18 @@ void alloc_cells(int l, int cells[l][l]) {
     }
 }
 
-int count_neighbours(int x, int y, int l, int cells[l][l]) {
+void alloc_gen(int cells[l][l], int gen[l][l]) {
+    for (int i=0; i<l; i++) {
+        for (int j=0; j<l; j++) {
+            cells[i][j] = gen[i][j];
+        }
+    }
+}
+
+int count_neighbours(int x, int y, int cells[l][l]) {
     int nei = 0;
-    /* printf("-----------\n"); */
     for (int i=-1; i<=1; i++) {
         for (int j=-1; j<=1; j++) {
-            /* printf("(%d, %d) ---- ", j, i); */
             if (cells[y + i][x + j] == 1 && (j != 0 || i != 0))
                 nei++;
         }
@@ -31,52 +42,49 @@ int count_neighbours(int x, int y, int l, int cells[l][l]) {
     return nei;
 }
 
-void update_cells(int x, int y, int nei, int l, int cells[l][l]) {
+void update_cells(int x, int y, int nei, int cells[l][l], int gen[l][l]) {
     if (nei < 2 && cells[y][x] == 1)
-        cells[y][x] = 0;
+        gen[y][x] = 0;
 
-    else if ((nei == 2 || nei == 3) && cells[y][x] == 1)
-        cells[y][x] = 1;
+    if ((nei == 2 || nei == 3) && cells[y][x] == 1)
+        gen[y][x] = 1;
 
-    else if (nei > 3 && cells[y][x] == 1)
-        cells[y][x] = 0;
+    if (nei > 3 && cells[y][x] == 1)
+        gen[y][x] = 0;
 
-    else if (nei == 3 && cells[y][x] == 0)
-        cells[y][x]= 1;
+    if (nei == 3 && cells[y][x] == 0)
+        gen[y][x]= 1;
 }
 
-void draw_cells(int x, int y, int size, int l, int cells[l][l]) {
-    if (cells[y][x] == 1)
+void draw_cells(int x, int y, int cells[l][l]) {
+    if (cells[y][x] == 1) // or gen
         DrawRectangle(x*size, y*size, size, size, YELLOW);
 }
 
 int main() {
-    const int L = 800;
-    const int size = 15;
-
-    const int l = (int)L/size;
-
     int cells[l][l];
+    int next_gen[l][l];
 
-    alloc_cells(l, cells);
+    alloc_cells(cells);
+    alloc_cells(next_gen);
 
     InitWindow(L, L, "GoL");
     SetTargetFPS(30);
 
-    /* cells[20][20] = 1; */
-    /* cells[21][21] = 1; */
-    /* cells[22][21] = 1; */
-    /* cells[22][20] = 1; */
-    /* cells[22][19] = 1; */
+    cells[20][20] = 1;
+    cells[21][21] = 1;
+    cells[22][21] = 1;
+    cells[22][20] = 1;
+    cells[22][19] = 1;
 
     /* cells[20][20] = 1; */
     /* cells[20][21] = 1; */
     /* cells[21][20] = 1; */
     /* cells[21][21] = 1; */
 
-    cells[19][20] = 1;
-    cells[20][20] = 1;
-    cells[21][20] = 1;
+    /* cells[19][20] = 1; */
+    /* cells[20][20] = 1; */
+    /* cells[21][20] = 1; */
 
     while (!WindowShouldClose()) {
         BeginDrawing();
@@ -86,14 +94,16 @@ int main() {
 
         for (int i=0; i<l; i++) {
             for (int j=0; j<l; j++) {
-                int nei = count_neighbours(j, i, l, cells);
+                int nei = count_neighbours(j, i, cells);
 
-                draw_cells(j, i, size, l, cells);
-                update_cells(j, i, nei, l, cells);
+                draw_cells(j, i, cells);
+                update_cells(j, i, nei, cells, next_gen);
             }
         }
 
-        draw_grid(L, size);
+        alloc_gen(cells, next_gen);
+
+        draw_grid();
 
         EndDrawing();
     }
