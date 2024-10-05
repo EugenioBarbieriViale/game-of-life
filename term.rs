c2414927bebbfffc6ca03af9a41d679674903ac6
EugenioBarbieriViale <eugenio.barbieri.viale@gmail.com>
2024-10-05 22:07:53 +0200

    new

--- term.rs
+++ term.rs
@@ -1,25 +1,56 @@
 // use rand::Rng;
 
-const WIDTH: usize = 10;
-const HEIGHT: usize = WIDTH * 2;
+const HEIGHT: usize = 10;
+const WIDTH: usize = HEIGHT * 2;
 
 fn main() {
-    let mut states = [[false; HEIGHT as usize]; WIDTH as usize];
+    let mut states = [[false; WIDTH]; HEIGHT];
 
-    states[5][5] = true;
+    let mut x: usize = 4;
+    let mut y: usize = 4;
 
-    draw_grid(&states);
+    let vel = 1;
+
+    loop {
+        if x < WIDTH-1 && y < HEIGHT-1 {
+            x += vel;
+        }
+
+        states[y][x] = true;
+
+        draw(&states);
+    }
 }
 
-fn draw_grid(states: &[[bool; HEIGHT as usize]; WIDTH as usize]) {
-    for row in 0..WIDTH {
-        for col in 0..HEIGHT {
-            if states[row][col] {
+fn count_neighbours(x: usize, y: usize, states: &[[bool; WIDTH]; HEIGHT]) -> usize {
+    let mut n = 0;
+
+    for j in 0..=2 {
+        for i in 0..=2 {
+            if x != 0 && y != 0 && x != WIDTH-1 && y != HEIGHT-1 {
+                let new_y: usize = y + (i as usize).abs_diff(1usize);
+                let new_x: usize = x + (j as usize).abs_diff(1usize);
+
+                if states[new_y][new_x] && (j != 1 || i != 1) {
+                    n += 1;
+                }
+            }
+        }
+    }
+    n
+}
+
+fn draw(states: &[[bool; WIDTH]; HEIGHT]) {
+    for y in 0..HEIGHT {
+        for x in 0..WIDTH {
+            if states[y][x] {
                 print!("@");
             }
             else {
                 print!(".");
             }
+
+            let n = count_neighbours(x, y, &states);
         }
         println!("");
     }
