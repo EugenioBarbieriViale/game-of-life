b67d4e82b3a67ce09e1d4aaca5e5c2d75831ff80
EugenioBarbieriViale <eugenio.barbieri.viale@gmail.com>
2024-10-06 19:00:47 +0200

    new

--- term.rs
+++ term.rs
@@ -1,39 +1,40 @@
-// use rand::Rng;
-
 const HEIGHT: usize = 10;
 const WIDTH: usize = HEIGHT * 2;
 
 fn main() {
+    let alive: char = '@';
+    let  dead: char = ' ';
+
     let mut states = [[false; WIDTH]; HEIGHT];
+    let mut next_states = states; 
 
     states[4][4] = true;
-
-    run(&states);
-}
-
-fn run(states: &[[bool; WIDTH]; HEIGHT]) {
+    states[4][5] = true;
+    states[4][6] = true;
+                
     loop {
         for y in 0..HEIGHT {
             for x in 0..WIDTH {
                 let n = count_neighbours(x, y, &states);
-
-                draw(x, y, &states);
+                compute_next(x, y, n, &states, &mut next_states);
+                draw(alive, dead, x, y, &states);
             }
             println!("");
         }
+        states = next_states;
     }
 }
 
 fn count_neighbours(x: usize, y: usize, states: &[[bool; WIDTH]; HEIGHT]) -> usize {
     let mut n = 0;
 
-    for j in 0..=2 {
-        for i in 0..=2 {
+    for j in -1i8..=1 {
+        for i in -1i8..=1 {
             if x != 0 && y != 0 && x != WIDTH-1 && y != HEIGHT-1 {
-                let new_y: usize = y + (i as usize).abs_diff(1usize);
-                let new_x: usize = x + (j as usize).abs_diff(1usize);
-
-                if states[new_y][new_x] && (j != 1 || i != 1) {
+                let new_y = i + (y as i8);
+                let new_x = j + (x as i8);
+                
+                if states[new_y as usize][new_x as usize] && (j != 0 || i != 0) {
                     n += 1;
                 }
             }
@@ -42,11 +43,31 @@ fn count_neighbours(x: usize, y: usize, states: &[[bool; WIDTH]; HEIGHT]) -> usi
     n
 }
 
-fn draw(x: usize, y: usize, states: &[[bool; WIDTH]; HEIGHT]) {
+fn compute_next(x: usize, y: usize, n: usize, 
+    states: &[[bool; WIDTH]; HEIGHT], next_states: &mut [[bool; WIDTH]; HEIGHT]) {
+
+    if n < 2 && states[y][x] {
+        next_states[y][x] = false;
+    }
+
+    if (n == 2 || n == 3) && states[y][x] {
+        next_states[y][x] = true;
+    }
+
+    if n > 3 && states[y][x] {
+        next_states[y][x] = false;
+    }
+
+    if n == 3 && !states[y][x] {
+        next_states[y][x] = true;
+    }
+}
+
+fn draw(alive: char, dead: char, x: usize, y: usize, states: &[[bool; WIDTH]; HEIGHT]) {
     if states[y][x] {
-        print!("@");
+        print!("{}", alive);
     }
     else {
-        print!(".");
+        print!("{}", dead);
     }
 }
